a5c4dfdd9b88fa13244ab103b7b9b10e8abb17e5
NotYoink <games.jarek@gmail.com>
2021-03-25 09:47:21 +0100

    optimising

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -94,10 +94,11 @@ public class Board : MonoBehaviour
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             int AMX = (int)Mathf.FloorToInt(mousePos.x + 0.5f);
             int AMY = (int)Mathf.FloorToInt(mousePos.y + 0.5f);
+            Transform child = squares[AMX, AMY].transform.GetChild(0);
 
             if (firstClick == true)
             {
-                if (squares[AMX, AMY].transform.childCount > 0 && squares[AMX, AMY].transform.GetChild(0).CompareTag(currentPlayer)) //clicking on own piece
+                if (squares[AMX, AMY].transform.childCount > 0 && child.CompareTag(currentPlayer)) //clicking on own piece
                 {
                     circlePrefab.transform.position = new Vector3(AMX, AMY, 0);
                     firstClick = false;
@@ -105,13 +106,13 @@ public class Board : MonoBehaviour
             }
             else
             {
-                if (squares[AMX, AMY].transform.childCount > 0 && squares[AMX, AMY].transform.GetChild(0).CompareTag(currentPlayer)) //clicking on own piece
+                if (squares[AMX, AMY].transform.childCount > 0 && child.CompareTag(currentPlayer)) //clicking on own piece
                 {
                     circlePrefab.transform.position = new Vector3(AMX, AMY, 0);
                 }
-                else if (squares[AMX, AMY].transform.childCount > 0 && !squares[AMX, AMY].transform.GetChild(0).CompareTag(currentPlayer)) //clicking on enemy piece
+                else if (squares[AMX, AMY].transform.childCount > 0 && !child.CompareTag(currentPlayer)) //clicking on enemy piece
                 {
-                    GameObject capturedPiece = squares[AMX, AMY].transform.GetChild(0).gameObject;
+                    GameObject capturedPiece = child.gameObject;
                     Destroy(capturedPiece);
                     squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).transform.position = new Vector3(AMX, AMY, 0);
                     squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).transform.SetParent(squares[AMX, AMY].transform, true);
@@ -127,15 +128,19 @@ public class Board : MonoBehaviour
                 {
                     if (circlePrefab.transform.position.x >= 0 && circlePrefab.transform.position.y >= 0 && circlePrefab.transform.position.x < 8 && circlePrefab.transform.position.y < 8)
                     {
-                        squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).transform.position = new Vector3(AMX, AMY, 0);
-                        squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).transform.SetParent(squares[AMX, AMY].transform, true);
-                        circlePrefab.transform.position = new Vector3(-10, -10, 0);
-                        firstClick = true;
-                        if (currentPlayer == "White")
+                        Piece legalMovesScript = squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).GetComponent<Piece>();
+                        if (legalMovesScript.legalMovesY.Contains((int)AMY - (int)circlePrefab.transform.position.y))
                         {
-                            currentPlayer = "Black";
+                            squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).transform.position = new Vector3(AMX, AMY, 0);
+                            squares[(int)circlePrefab.transform.position.x, (int)circlePrefab.transform.position.y].transform.GetChild(0).transform.SetParent(squares[AMX, AMY].transform, true);
+                            circlePrefab.transform.position = new Vector3(-10, -10, 0);
+                            firstClick = true;
+                            if (currentPlayer == "White")
+                            {
+                                currentPlayer = "Black";
+                            }
+                            else currentPlayer = "White";
                         }
-                        else currentPlayer = "White";
                     }
                 }
             }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Piece : MonoBehaviour
 {
     public bool white;
-    public List<int> legalMoves;
+    public List<int> legalMovesX;
+    public List<int> legalMovesY;
 
     void Start()
     {
@@ -13,12 +14,25 @@ public class Piece : MonoBehaviour
         {
             for (int i = 1; i < 8; i++)
             {
-                legalMoves.Add(i);
+                legalMovesY.Add(i);
+            }
+        }
+        if (gameObject.name == "Rook(Clone)" && !white)
+        {
+            for (int i = 1; i < 8; i++)
+            {
+                legalMovesY.Add(-i);
             }
         }
         if (gameObject.name == "Pawn(Clone)" && white)
         {
-            legalMoves.Add(1);
+            legalMovesY.Add(1);
+            legalMovesX.Add(0);
+        }
+        if (gameObject.name == "Pawn(Clone)" && !white)
+        {
+            legalMovesY.Add(-1);
+            legalMovesX.Add(0);
         }
     }
 }
